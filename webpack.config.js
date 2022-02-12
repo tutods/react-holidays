4b747b059a4ef9b85732731257a0ebee56123cf4
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-02-12 17:57:45 +0000

    Fix error when make a manual refresh

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
 	entry: path.resolve(__dirname, 'src', 'index.tsx'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		publicPath: '/'
 	},
 	mode: 'development',
 	module: {
